bd833142e6ef9260aaad12a7d03d2044e589025c
Paweł Woliński <pawel.wolinski@op.pl>
2018-11-25 22:22:34 +0100

    Basic LS class structure created

--- LearnSmarter.Mobile.Core/Models/LearningSubject.cs
+++ LearnSmarter.Mobile.Core/Models/LearningSubject.cs
@@ -6,5 +6,17 @@ namespace LearnSmarter.Mobile.Core.Models
 {
     public class LearningSubject
     {
+        public string Name { get; private set; }
+        public string Category { get; private set; }
+        public string Description { get; set; }
+        public List<Note> Notes { get; private set; }
+        public List<Repetition> Repetitions { get; private set; }
+        public Repetition NextRepetition { get; private set; }
+        public DateTime? Deadline { get; private set; }
+
+        public LearningSubject()
+        {
+
+        }
     }
 }
---
+++ LearnSmarter.Mobile.Core/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnSmarter.Mobile.Core.Models
+{
+    public class Note
+    {
+        public string Content { get; set; }
+        public List<string> Tags { get; set; }
+
+        public Note(string content)
+        {
+            Content = content;
+        }
+    }
+}
