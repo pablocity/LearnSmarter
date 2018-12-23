a2d0525ebccc0ece7c91bdae8e197247a97d4b5e
Paweł Woliński <pawel.wolinski@op.pl>
2018-12-23 23:47:59 +0100

    Added Ratings enum

---
+++ LearnSmarter.Mobile.Core/Models/Ratings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnSmarter.Mobile.Core.Models
+{
+    public enum Ratings
+    {
+        VeryEasy,
+        Easy,
+        Medium,
+        Hard,
+        VeryHard
+    }
+}
