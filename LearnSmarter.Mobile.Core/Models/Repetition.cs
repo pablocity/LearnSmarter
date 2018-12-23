d3ad88fdb51df00e21e1dcc98270ed785470910a
Paweł Woliński <pawel.wolinski@op.pl>
2018-12-23 23:45:11 +0100

    Added basic structure of the Repetition class

--- LearnSmarter.Mobile.Core/Models/Repetition.cs
+++ LearnSmarter.Mobile.Core/Models/Repetition.cs
@@ -6,5 +6,11 @@ namespace LearnSmarter.Mobile.Core.Models
 {
     public class Repetition
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime? NextDate { get; set; }
+        public DateTime TimeAmount { get; set; }
+        public Ratings Rating { get; set; }
     }
 }
