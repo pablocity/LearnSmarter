d85df4f729ecb0193fc41639e053d693848f1d01
Paweł Woliński <pawel.wolinski@op.pl>
2019-01-24 18:36:23 +0100

    HomeViewModel partially adapted to proper LS model class

--- LearnSmarter.Mobile.Core/ViewModels/HomeViewModel.cs
+++ LearnSmarter.Mobile.Core/ViewModels/HomeViewModel.cs
@@ -1,15 +1,28 @@
-﻿using MvvmCross.Commands;
+﻿using LearnSmarter.Mobile.Core.Models;
+using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace LearnSmarter.Mobile.Core.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
         public IMvxCommand ResetTextCommand => new MvxCommand(ResetText);
+        public IMvxAsyncCommand AddLSCommand => new MvxAsyncCommand(AddLS);
+
         private void ResetText()
         {
             Text = "Hello MvvmCross";
+        }
+
+        public async Task AddLS()
+        {
+            int i = 0;
+            LearningSubject result = await NavigationService.Navigate<LearningSubject>(typeof(LSAddViewModel));
+
+            if (result != null)
+                Collection.Add(result);
         }
 
         private string _text = "Hello MvvmCross";
@@ -19,18 +32,11 @@ namespace LearnSmarter.Mobile.Core.ViewModels
             set { SetProperty(ref _text, value); }
         }
 
-        public ObservableCollection<string> Collection { get; set; }
+        public ObservableCollection<LearningSubject> Collection { get; set; }
 
         public HomeViewModel()
         {
-            Collection = new ObservableCollection<string>()
-            {
-                "1",
-                "2",
-                "3",
-                "4",
-                "5",
-            };
+            Collection = new ObservableCollection<LearningSubject>();
         }
     }
 }
