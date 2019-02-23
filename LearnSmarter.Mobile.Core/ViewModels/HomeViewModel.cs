2e8520a8ba2760c59b4fb0704cd436bb1dce64dd
Paweł Woliński <pawel.wolinski@op.pl>
2019-02-23 15:39:57 +0100

    Cleaning up old test properties

--- LearnSmarter.Mobile.Core/ViewModels/HomeViewModel.cs
+++ LearnSmarter.Mobile.Core/ViewModels/HomeViewModel.cs
@@ -8,35 +8,25 @@ namespace LearnSmarter.Mobile.Core.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
-        public IMvxCommand ResetTextCommand => new MvxCommand(ResetText);
         public IMvxAsyncCommand AddLSCommand => new MvxAsyncCommand(AddLS);
-
-        private void ResetText()
-        {
-            Text = "Hello MvvmCross";
-        }
 
         public async Task AddLS()
         {
-            int i = 0;
             LearningSubject result = await NavigationService.Navigate<LearningSubject>(typeof(LSAddViewModel));
 
             if (result != null)
                 Collection.Add(result);
         }
 
-        private string _text = "Hello MvvmCross";
-        public string Text
-        {
-            get { return _text; }
-            set { SetProperty(ref _text, value); }
-        }
-
         public ObservableCollection<LearningSubject> Collection { get; set; }
 
         public HomeViewModel()
         {
-            Collection = new ObservableCollection<LearningSubject>();
+            Collection = new ObservableCollection<LearningSubject>()
+            {
+                new LearningSubject("Nazwa", System.DateTime.Now)
+            };
+
         }
     }
 }
