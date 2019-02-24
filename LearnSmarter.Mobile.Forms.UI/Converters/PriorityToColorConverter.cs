a7cbd6042c5b20954d573c93e8d7a82a301f1fcd
Paweł Woliński <pawel.wolinski@op.pl>
2019-02-24 14:05:21 +0100

    Implemented PriorityToColor converter

---
+++ LearnSmarter.Mobile.Forms.UI/Converters/PriorityToColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using LearnSmarter.Mobile.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace LearnSmarter.Mobile.Forms.UI.Converters
+{
+    public class PriorityToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Priority priority))
+                throw new ArgumentException("Expected Priority enumeration type!");
+
+            switch(priority)
+            {
+                case Priority.Average:
+                    return "#3498db";
+                case Priority.Important:
+                    return "#f39c12";
+                case Priority.Low:
+                    return "#5bb85d";
+                default:
+                    return "#e74c3c";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
