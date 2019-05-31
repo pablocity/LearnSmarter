3533b69d7d7ef4b7257172d0ac3db2ff7179a21f
Paweł Woliński <pawel.wolinski@op.pl>
2019-06-01 00:44:25 +0200

    Grouping class for expandable listView added

---
+++ LearnSmarter.Mobile.Core/Models/Group.cs
@@ -0,0 +1,87 @@
+﻿using LearnSmarter.Mobile.Core.ViewModels;
+using MvvmCross.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace LearnSmarter.Mobile.Core.Models
+{
+    public class Group<TKey, TElement> : ObservableCollection<TElement>
+    {
+        public IMvxCommand ExpandOrCollapseCommand => new MvxCommand(ExpandOrCollapse);
+        public TKey Key { get; private set; }
+
+        private List<TElement> lastState;
+        private readonly HomeViewModel vm;
+
+        private bool isExpanded;
+        public bool IsExpanded
+        {
+            get
+            {
+                return isExpanded;
+            }
+            set
+            {
+                isExpanded = value;
+
+            }
+        }
+
+        public Group(TKey key, List<TElement> elements)
+        {
+            Key = key;
+            lastState = new List<TElement>(elements);
+            IsExpanded = false;
+            //elements.ForEach(x => Items.Add(x));
+        }
+
+        public Group(TKey key, List<TElement> elements, HomeViewModel viewModel)
+        {
+            Key = key;
+            lastState = new List<TElement>(elements);
+            IsExpanded = false;
+            vm = viewModel;
+            //elements.ForEach(x => Items.Add(x));
+        }
+
+        private void Expand()
+        {
+            foreach (TElement item in lastState)
+            {
+                Items.Add(item);
+            }
+
+            IsExpanded = true;
+
+            Group<LearningSubject, Repetition> ls = vm.Subjects[vm.Subjects.Count - 1];
+            vm.Subjects.RemoveAt(vm.Subjects.Count - 1);
+            vm.Subjects.Add(ls);
+            vm.RaisePropertyChanged("Subjects");
+            
+        }
+
+        private void Collapse()
+        {
+            lastState = new List<TElement>(Items);
+            Items.Clear();
+            IsExpanded = false;
+
+            Group<LearningSubject, Repetition> ls = vm.Subjects[vm.Subjects.Count - 1];
+            vm.Subjects.RemoveAt(vm.Subjects.Count - 1);
+            vm.Subjects.Add(ls);
+
+            vm.RaisePropertyChanged("Subjects");
+        }
+
+        private void ExpandOrCollapse()
+        {
+            if (IsExpanded)
+                Collapse();
+            else
+                Expand();
+        }
+    }
+}
